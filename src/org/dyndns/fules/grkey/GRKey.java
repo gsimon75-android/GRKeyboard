35280c8f6cafd27302343269f43b3f852c12eaad
Gabor Simon <gsimon75@gmail.com>
2016-12-25 09:14:34 +0000

    drawable state handling works
    git-svn-id: file:///home/fules/svn/android/GRKeyboard/trunk@1037 ac99d0f3-1a01-e011-8a23-002586f6d316

--- src/org/dyndns/fules/grkey/GRKey.java
+++ src/org/dyndns/fules/grkey/GRKey.java
@@ -9,6 +9,7 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.Shader.TileMode;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.text.InputType;
 import android.util.AttributeSet;
@@ -30,6 +31,9 @@ import java.util.Iterator;
 public class GRKey extends Button {
 	// Constants
 	private static final String		TAG = "GRKeyboard";
+	private GRKeyboardService		svc;
+	private int[] stateNormal		= { android.R.attr.state_enabled, android.R.attr.state_window_focused, android.R.attr.state_multiline };
+	private int[] statePressed		= { android.R.attr.state_enabled, android.R.attr.state_window_focused, android.R.attr.state_multiline, android.R.attr.state_pressed };
 
 	public GRKey(Context context) {
 		super(context);
@@ -38,26 +42,76 @@ public class GRKey extends Button {
 
 	public GRKey(Context context, AttributeSet attributes) {
 		super(context, attributes);
+		Log.d(TAG, "GRKey(" + context + ", " + attributes + ")");
+		//dumpAttributeSet(attributes);
 		init(context);
 	}
 
-	public GRKey(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
+	public GRKey(Context context, AttributeSet attributes, int defStyleAttr) {
+		super(context, attributes, defStyleAttr);
+		Log.d(TAG, "GRKey(" + context + ", " + attributes + ", " + defStyleAttr + ")");
+		//dumpAttributeSet(attributes);
 		init(context);
+	}
+
+	void dumpAttributeSet(AttributeSet attrs) {
+		for (int i = 0; i < attrs.getAttributeCount(); ++i) {
+			Log.d(TAG, "attr; name='" + attrs.getAttributeName(i) + "', value='" + attrs.getAttributeValue(i) + "'");
+		}
 	}
 
 	void init(Context context) {
 		if (context instanceof GRKeyboardService) {
-			final GRKeyboardService svc = (GRKeyboardService)context;
-			setOnClickListener(new View.OnClickListener() {
-				public void onClick(View v) {
-					svc.keyClicked(v);
-				}
-			});
+			svc = (GRKeyboardService)context;
 		}
 		else {
 			Log.d(TAG, "Context of key is not a GRKeyboardService");
 		}
+		/*setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				svc.keyClicked(v);
+			}
+		});
+		setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				svc.keyClicked(v);
+			}
+		});*/
+	}
+
+	@Override public boolean onTouchEvent(MotionEvent event) {
+		Log.d(TAG, "onTouchEvent(" + event + ")");
+		switch (event.getAction()) {
+			case MotionEvent.ACTION_DOWN: {
+				Drawable bg = getBackground();
+				for (int s : bg.getState()) {
+					Log.d(TAG, "onTouchEvent; bg state=" + s);
+				}
+				if (bg.setState(statePressed)) {
+					Log.d(TAG, "onTouchEvent; setState returned true");
+					bg.invalidateSelf();
+				}
+			}
+			break;
+
+			case MotionEvent.ACTION_MOVE:
+				break;
+
+			case MotionEvent.ACTION_UP: {
+				Drawable bg = getBackground();
+				for (int s : bg.getState()) {
+					Log.d(TAG, "onTouchEvent; bg state=" + s);
+				}
+				if (bg.setState(stateNormal)) {
+					Log.d(TAG, "onTouchEvent; setState returned true");
+					bg.invalidateSelf();
+				}
+				if (svc != null)
+					svc.keyClicked(this);
+			}
+			break;
+		}
+		return true;
 	}
 
 }
