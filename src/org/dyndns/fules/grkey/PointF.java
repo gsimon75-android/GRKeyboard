55965e4a499d6823325c16ef1fb903022f95e44f
Gabor Simon <gsimon75@gmail.com>
2017-02-04 14:26:49 +0000

    GestureView fetched from test
    git-svn-id: file:///home/fules/svn/android/GRKeyboard/trunk@1096 ac99d0f3-1a01-e011-8a23-002586f6d316

--- src/org/dyndns/fules/grkey/PointF.java
+++ src/org/dyndns/fules/grkey/PointF.java
@@ -1,25 +1,63 @@
 package org.dyndns.fules.grkey;
 
 class PointF {
-	public static final float PI = (float)Math.PI;
+    public static final float PI = (float)Math.PI;
 
-	float x, y;
+    float x, y;
 
-	PointF() {
-		this(0, 0);
-	}
+    PointF() {
+        this(0, 0);
+    }
 
-	PointF(float x, float y) {
-		this.x = x;
-		this.y = y;
-	}
+    PointF(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 
-	public final String toString() {
-		return "(" + x + ", " + y + ")";
-	}
+    PointF(PointF other) {
+        x = other.x;
+        y = other.y;
+    }
 
-	public float abs2() {
-		return x*x + y*y;
-	}
+    public final String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public float abs2() {
+        return x*x + y*y;
+    }
+
+    public void add(PointF other) {
+        x += other.x;
+        y += other.y;
+    }
+
+    public void subtract(PointF other) {
+        x -= other.x;
+        y -= other.y;
+    }
+
+    public void multiply(float r) {
+        x *= r;
+        y *= r;
+    }
+
+    public void normalise() {
+        float l = (float)Math.sqrt(abs2());
+        if (l != 0) {
+            x /= l;
+            y /= l;
+        }
+    }
+
+    public void makeLength(float r) {
+        float l = (float)Math.sqrt(abs2());
+        if (l != 0) {
+            x = x * r / l;
+            y = y * r / l;
+        }
+    }
+
 }
 
+// vim: set ts=4 sw=4 et:
